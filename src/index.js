40697e93e2474a6aeeb81f2e0a15e930c2ea4fd2
Alexey Yermakov <yermakov.alexx@gmail.com>
2020-10-03 18:49:17 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (arguments.length && array.length) {
+    return Math.min(...array);
+  } else return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (arguments.length && array.length) {
+    return Math.max(...array);
+  } else return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (arguments.length && array.length) {
+    return array.reduce((total, amount, index, array) => {
+      total += amount;
+      if( index === array.length-1) { 
+        return total/array.length;
+      }else { 
+        return total;
+      }
+    }, 0);
+  } else return 0;
 }
